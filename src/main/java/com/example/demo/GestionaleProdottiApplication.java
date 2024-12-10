ee2445cd98c0860f9ffde4424d22af2c2c652b25
Giuseppe Saccone <141818801+peppe813@users.noreply.github.com>
2024-12-10 15:32:33 +0100

    primo commit

---
+++ src/main/java/com/example/demo/GestionaleProdottiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionaleProdottiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionaleProdottiApplication.class, args);
+	}
+
+}
